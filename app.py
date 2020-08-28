da8f2c1150243c9789cba664718ee654f3b84459
rokyunghim <rokyunghim@gmail.com>
2020-08-28 17:07:00 +0900

    update

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ from flask import Flask, render_template, jsonify, request,session
 app = Flask(__name__)
 
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@3.35.52.217', 27017)
 db = client.feel
 app.secret_key = "ABCDEFG"
 
 @app.route('/')
+def main():
+    if "username" in session:
+        return render_template('profil.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/signin')
 def signIn():
     if "username" in session:
         return render_template('profil.html')
@@ -71,12 +80,30 @@ def record():
     else:
         return render_template('signin.html')
 
+def objectIdDecoder(list):
+  results=[]
+  for document in list:
+    document['_id'] = str(document['_id'])
+    results.append(document)
+  return results
 
 @app.route('/record-get', methods=['GET'])
 def recordGet():
     username = session['username']
-    data = list(db.contents.find({'username': username}, {'_id': False}))
+    data = objectIdDecoder(list(db.contents.find({'username': username}).sort('number',-1)))
     return jsonify(data)
+
+@app.route('/all-get', methods=['GET'])
+def allGet():
+    data = objectIdDecoder(list(db.contents.find({}).sort('number',-1)))
+    return jsonify(data)
+
+@app.route('/random', methods=['GET'])
+def random():
+    if "username" in session:
+        return render_template('random.html')
+    else:
+        return render_template('signin.html')
 
 
 @app.route('/record-post', methods=['POST'])
@@ -84,15 +111,33 @@ def recordPost():
     why = request.form['why']
     stat = request.form['stat']
     time = request.form['time']
+    number = request.form['number']
     username = session['username']
+
     print(why, stat, time)
-    doc = {'stat': stat, 'why': why, 'time': time, 'username' : username}
+    doc = {'stat': stat, 'why': why, 'time': time, 'username' : username,'number':number}
+    print(doc)
     db.contents.insert_one(doc)
     return jsonify({'hi': 'hi'})
 
+@app.route('/delete', methods=['POST'])
+def deleteDb():
+    ID = request.form['id']
+    print(ID)
+    db.contents.delete_one({'_id': ObjectId(ID)})
+    return jsonify({'result':'success'})
+
+@app.route('/update', methods=['POST'])
+def updateDb():
+    ID = request.form['id']
+    new_content = request.form['content']
+    print(ID,new_content)
+    db.contents.update({'_id': ObjectId(ID)},{'$set': {'why': new_content}})
+    return jsonify({'result':'success'})
+
 @app.route("/out")
 def session_out():
-    session.clear()
+    session.pop("username", None)
     return 'bye member!'
 
 
